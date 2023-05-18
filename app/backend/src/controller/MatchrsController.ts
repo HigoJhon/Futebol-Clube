deb0ee2221849a7319db5fa81cefc46c3e0aa0f8
HigoJhon <higojhon98@gmail.com>
2023-05-18 17:13:11 -0300

    Criando endpoint get "inProgress"

--- app/backend/src/controller/MatchrsController.ts
+++ app/backend/src/controller/MatchrsController.ts
@@ -3,13 +3,15 @@ import MatchersService from '../service/MatchesService';
 
 export default class MatchesController {
   private _matchesService = new MatchersService();
-  async getAll(_req: Request, res: Response): Promise<Response> {
-    // try {
-    const { type, message } = await this._matchesService.getAll();
-    console.log('cheguei na controller assim:', message);
-    return res.status(type).json(message);
-    // } catch (error) {
-    //   return res.status(500).json({ message: 'internal error' });
-    // }
+  async getAll(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+    try {
+      const { type, message } = (inProgress)
+        ? await this._matchesService.getInProgress(inProgress as string)
+        : await this._matchesService.getAll();
+      return res.status(type).json(message);
+    } catch (error) {
+      return res.status(500).json({ message: 'internal error' });
+    }
   }
 }
--- app/backend/src/service/MatchesService.ts
+++ app/backend/src/service/MatchesService.ts
@@ -10,7 +10,17 @@ export default class TeamsService {
         { model: Team, as: 'awayTeam', attributes: { exclude: ['id'] } },
       ],
     });
-    // console.log('cheguei no service assim:', matchers);
+    return { type: 200, message: matchers };
+  };
+
+  public getInProgress = async (inProgress: string) => {
+    const matchers = await Matchers.findAll({
+      where: { inProgress: inProgress === 'true' },
+      include: [
+        { model: Team, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    });
     return { type: 200, message: matchers };
   };
 }
