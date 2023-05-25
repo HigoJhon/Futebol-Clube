69698c120a3cf023e55b12205aec6af9072cc873
HigoJhon <higojhon98@gmail.com>
2023-05-24 22:52:14 -0300

    completando mas testes

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -6,10 +6,14 @@ import chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
 // import { app } from '../app';
-import teamModel from '../database/models/Teams';
 import { Response } from 'superagent';
+import teamModel from '../database/models/Teams';
 import TeamsService from '../service/TeamsService';
 import TeamsController from '../controller/TeamsController';
+
+import LoginModel from '../database/models/Users';
+import LoginService from '../service/LoginService';
+import LoginController from '../controller/LoginConntroller';
 
 chai.use(chaiHttp);
 
@@ -18,6 +22,9 @@ const { expect } = chai;
 const _teamsService = new TeamsService();
 const _teamsController = new TeamsController();
 
+const _loginService = new LoginService();
+const _loginController = new LoginController();
+
 describe('Desenvolvendo test para a rota de Teams', () => {
     afterEach(() => sinon.restore());
   it('buscando todos os times de service', async () => {
@@ -25,6 +32,14 @@ describe('Desenvolvendo test para a rota de Teams', () => {
 
     _teamsService.getAll();
   });
+
+  it('buscando times por Id', async () => {
+    const id = 1;
+    sinon.stub(teamModel, 'findOne').resolves();
+
+    _teamsService.getById(id);
+  });
+
   it('buscando todos os times de controller', async () => {
     const req: any = {};
     const res: any = {
@@ -37,3 +52,39 @@ describe('Desenvolvendo test para a rota de Teams', () => {
   }
   );
 });
+
+describe('Desenvolvendo test para a rota de Login', () => {
+  afterEach(() => sinon.restore());
+  it('testando o getAll de login', async () => {
+    const req: any = {};
+    const res: any = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+    };
+    sinon.stub(LoginModel, 'findOne').resolves();
+
+    await _loginController.get(req, res);
+  });
+
+  it('testando o post de login', async () => {
+    const req: any = {};
+    const res: any = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+    };
+    sinon.stub(LoginModel, 'findOne').resolves();
+
+    await _loginController.post(req, res);
+  });
+
+  it('testando o getAll de login na service', async () => {
+    const req: any = {};
+    const res: any = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub().returnsThis(),
+    };
+    sinon.stub(LoginModel, 'findOne').resolves();
+
+    _loginService.get(req);
+  });
+})
